7615c5a58bedf2683b0989553f722db00e898f21
Elabbade Mouad <elabbademouad@gmail.com>
2018-10-05 18:32:17 +0100

    create List page

--- 01-Presentation/ThingsYouMustKnow/src/pages/list/list.ts
+++ 01-Presentation/ThingsYouMustKnow/src/pages/list/list.ts
@@ -6,32 +6,23 @@ import { NavController, NavParams } from 'ionic-angular';
   templateUrl: 'list.html'
 })
 export class ListPage {
-  selectedItem: any;
-  icons: string[];
-  items: Array<{title: string, note: string, icon: string}>;
+  items: Array<{title: string, date: string, image: string,text: string}>;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
-    // If we navigated to this page, we will have an item available as a nav param
-    this.selectedItem = navParams.get('item');
-
-    // Let's populate this page with some filler content for funzies
-    this.icons = ['flask', 'wifi', 'beer', 'football', 'basketball', 'paper-plane',
-    'american-football', 'boat', 'bluetooth', 'build'];
-
     this.items = [];
-    for (let i = 1; i < 11; i++) {
-      this.items.push({
-        title: 'Item ' + i,
-        note: 'This is item #' + i,
-        icon: this.icons[Math.floor(Math.random() * this.icons.length)]
-      });
+    this.items.push({
+        title :"5 things must to know about algorithm",
+        date : "November 5, 1955",
+        image :"https://assets.justinmind.com/blog/wp-content/uploads/2016/10/best-fonts-for-apps-fonts-for-web-ux-design-typography.png",
+        text :"Wait a minute. Wait a minute, Doc. Uhhh... Are you telling me that you built a time machine... out of a DeLorean?! Whoa. This is heavy."
+      }
+    );
+    this.items.push({
+      title :"How many weights does the font have?",
+      date : "October 5, 2017",
+      image :"https://assets.justinmind.com/blog/wp-content/uploads/2016/10/best-fonts-for-apps-typography-ux-design.png",
+      text :"If a font has a wide selection of weight, a large x-height and can scale well then, it’s likely that it’ll work well in most layouts, context permitting.When deciding on typography in a project, your user’s needs should be at the forefront of your mind. After all, it is your users who you’re designing for. If you’ve designed a mobile app focused on learning, then you’ll have to choose a font to reflect that. Something flexible and functional. Style over substance. And just because you like a font, doesn’t mean it’s going to work for others."
     }
-  }
-
-  itemTapped(event, item) {
-    // That's right, we're pushing to ourselves!
-    this.navCtrl.push(ListPage, {
-      item: item
-    });
+  );
   }
 }
